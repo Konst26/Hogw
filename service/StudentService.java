6fc1b26b868e59639f0f0ac5c698709c7ad05dd1
osoki <osokinkonstantin@mail.ru>
2024-11-26 20:49:09 +0300

    Выполненная работа

--- service/StudentService.java
+++ service/StudentService.java
@@ -1,11 +1,13 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+@Service
 public class StudentService {
 
     private final HashMap<Long, Student> students = new HashMap<>();
